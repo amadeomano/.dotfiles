89b167e11101aeccc5a27492eac9c08c7f9b4a0d
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2020-05-08 12:51:20 +0200

    Added git related shortcut aliases

--- .zshrc-modules/.aliases.zsh
+++ .zshrc-modules/.aliases.zsh
@@ -3,3 +3,10 @@ alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # Python install shortcut
 alias pi='python3 -m pip install --user $@'
+
+# Git related aliases
+alias gs='git status'
+alias ga='git add %@'
+alias gc='git commit %@'
+alias gp='git push %@'
+alias gr='git rebase %@'
