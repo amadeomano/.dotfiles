bf5ab995a74f8806a0ee5cf9eadbafc2c07afd72
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2023-04-15 16:00:04 +0200

    remap telescope to Cp, Ca, k and j

--- .config/nvim/lua/modules/plugins/telescope.lua
+++ .config/nvim/lua/modules/plugins/telescope.lua
@@ -5,12 +5,13 @@ local def = {
 
 def.configure = function()
   -- [[ Configure Telescope ]]
+  local actions = require "telescope.actions"
   require('telescope').setup {
     defaults = {
       mappings = {
         i = {
-          ['<C-u>'] = false,
-          ['<C-d>'] = false,
+          ['<C-j>'] = actions.move_selection_next,
+          ['<C-k>'] = actions.move_selection_previous,
         },
       },
     },
@@ -31,10 +32,10 @@ def.bindKeys = function()
     })
   end, { desc = '[/] Fuzzily search in current buffer' })
 
-  vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
   vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
   vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+  vim.keymap.set('n', '<C-a>', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
   vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 end
 
