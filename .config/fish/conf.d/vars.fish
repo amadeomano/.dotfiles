0ea3866650568cc8bcecdfacb275b70a1a61ecf7
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2023-07-24 09:33:58 +0200

    install joshmedeski/t-smart-tmux-session-manager

--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -4,3 +4,7 @@ set fish_greeting
 # set config path
 set -x XDG_CONFIG_HOME "$HOME/.config"
 
+# add bins to paths
+fish_add_path $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin
+fish_add_path /opt/homebrew/bin/
+fish_add_path /opt/homebrew/sbin/
