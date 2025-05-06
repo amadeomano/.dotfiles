01ab6f42827893fb99744a1dabf89025e6140dbd
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2025-05-06 09:34:37 +0200

    feat: update nvim deps and config dashboard

---
+++ .config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        graphql = { "prettier" }, -- prettier for graphql formatting
+      },
+      formatters = {
+        prettier = {
+          options = {
+            ext_parsers = {
+              query = "graphql", -- graphql parser for .query files
+            },
+          },
+        },
+      },
+    },
+  },
+}
