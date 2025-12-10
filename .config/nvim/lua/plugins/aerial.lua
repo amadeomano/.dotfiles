50bf651bc75d892f4a51fe8cbc0f4adc23de9619
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2025-12-10 12:42:03 +0000

    feat: add variables to typescript aerial

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,3 +1,26 @@
+local default_ts = {
+  "Class",
+  "Constructor",
+  "Enum",
+  "Field",
+  "Function",
+  "Interface",
+  "Method",
+  "Module",
+  "Namespace",
+  "Package",
+  "Property",
+  "Struct",
+  "Trait",
+}
+
+local function with_kind(base, ...)
+  local result = {}
+  vim.list_extend(result, base)
+  vim.list_extend(result, { ... })
+  return result
+end
+
 return {
   "stevearc/aerial.nvim",
   opts = {
@@ -7,6 +30,7 @@ return {
     },
     filter_kind = {
       typescriptreact = false,
+      typescript = with_kind(default_ts, "Variable"),
     },
     layout = {
       min_width = { 30, 0.15 },
