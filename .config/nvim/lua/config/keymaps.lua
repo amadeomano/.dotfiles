13b2b275ad6e87efba2fd55b834824c6da58990b
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2025-12-16 13:51:50 +0100

    feat: add cmd C/P to neovide

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle<CR>")
 
+-- Toggle inline Diagnostics
 vim.keymap.set("n", "<leader>ud", function()
   local on_state = {
     spacing = 4,
@@ -18,3 +19,19 @@ vim.keymap.set("n", "<leader>ud", function()
     vim.diagnostic.config({ virtual_text = false })
   end
 end, { desc = "Toggle Inline Diagnostics" })
+
+-- Clipboard access in neovide
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
