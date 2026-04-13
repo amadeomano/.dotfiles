fea71366fd830cc2f67b2b1534b8bd03c22e0d55
Amadeo Manouchehri <amadeo.manouchehri@gmail.com>
2026-04-13 16:48:29 +0200

    feat: add fontsize scale and path copy to keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -28,7 +28,19 @@ if vim.g.neovide then
   vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
   vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+  -- Font size via scale factor
+  vim.g.neovide_scale_factor = 1.0
+  local scale = function(d) vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * d end
+
+  vim.keymap.set("n", "<D-=>", function() scale(1.1) end, { desc = "Increase font size" })
+  vim.keymap.set("n", "<D-->", function() scale(1 / 1.1) end, { desc = "Decrease font size" })
+  vim.keymap.set("n", "<D-0>", function() vim.g.neovide_scale_factor = 1.0 end, { desc = "Reset font size" })
 end
+
+-- Copy buffer path to clipboard
+vim.keymap.set("n", "<leader>cp", function() vim.fn.setreg("+", vim.fn.expand("%:.")) end, { desc = "Copy relative path" })
+vim.keymap.set("n", "<leader>cP", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Copy absolute path" })
 
 -- Allow clipboard copy paste in neovim
 vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
